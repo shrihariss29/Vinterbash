3cb507ed95dbde1bbef4b925201e2797adca81cb
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-06-06 12:11:54 +0530

    Update data.js

--- src/Components/data.js
+++ src/Components/data.js
@@ -1,37 +1,128 @@
 const data = [
-    {
-      position: '1',
-      name: 'School1 Name',
-      points: '100',
-      events: [
-        { name: 'Event1', points: '15' },
-        { name: 'Event2', points: '25' },
-        { name: 'Event3', points: '30' },
-        { name: 'Event4', points: '30' },
-      ],
-    },
-    {
-      position: '2',
-      name: 'School2 Name',
-      points: '90',
-      events: [
-        { name: 'Event1', points: '30' },
-        { name: 'Event2', points: '30' },
-        { name: 'Event3', points: '30' },
-      ],
-    },
-    {
-      position: '3',
-      name: 'School3 Name',
-      points: '85',
-      events: [
-        { name: 'Event1', points: '10' },
-        { name: 'Event2', points: '25' },
-        { name: 'Event3', points: '25' },
-        { name: 'Event4', points: '25' },
-      ],
-    },
-  ];
-  
-  export default data;
-  
+  {
+    position: '1',
+    name: 'Kamala Nikethan Montessari School',
+    points: '100',
+    events: [
+      { name: 'Dance - III', points: '15' },
+      { name: 'Drama - I', points: '25' },
+      { name: 'Quiz - I', points: '30' },
+      { name: 'Singing - III', points: '30' },
+    ],
+  },
+  {
+    position: '2',
+    name: 'Sri Renga Matriculation',
+    points: '90',
+    events: [
+      { name: 'Dance - I', points: '25' },
+      { name: 'Singing - I', points: '25' },
+      { name: 'Quiz - II', points: '15' },
+      { name: 'Cooking - III', points: '25' },
+    ],
+  },
+  {
+    position: '3',
+    name: 'Sri Vageesha Vidhyashram',
+    points: '85',
+    events: [
+      { name: 'Drawing - II', points: '15' },
+      { name: 'Drama - III', points: '10' },
+      { name: 'Quiz - III', points: '10' },
+      { name: 'Singing - II', points: '50' },
+    ],
+  },
+  {
+    position: '4',
+    name: 'Cauvery Global',
+    points: '80',
+    events: [
+      { name: 'Shortfilm - I', points: '25' },
+      { name: 'Spoof - II', points: '10' },
+      { name: 'Drawing - III', points: '15' },
+      { name: 'Title event - I', points: '30' },
+    ],
+  },
+  {
+    position: '5',
+    name: 'RSK CBSE School',
+    points: '75',
+    events: [
+      { name: 'Title event - II', points: '15' },
+      { name: 'Drawing - I', points: '15' },
+      { name: 'Shortfilm - III', points: '10' },
+      { name: 'Spoof - I', points: '35' },
+    ],
+  },
+  {
+    position: '6',
+    name: 'Alpha senior secondary school',
+    points: '70',
+    events: [
+      { name: 'Quiz - II', points: '25' },
+      { name: 'Cooking - II', points: '25' },
+      { name: 'Title event - III', points: '20' },
+    ],
+  },
+  {
+    position: '7',
+    name: 'Srirangam BHSS School',
+    points: '65',
+    events: [
+      { name: 'Dance - II', points: '25' },
+      { name: 'Spoof - III', points: '15' },
+      { name: 'Shortfilm - II', points: '25' },
+    ],
+  },
+  {
+    position: '8',
+    name: 'Sri Akilandeswari Vidhyalaya',
+    points: '60',
+    events: [
+      { name: 'Quiz - III', points: '10' },
+      { name: 'Singing - I', points: '30' },
+      { name: 'Spoof - II', points: '20' },
+    ],
+  },
+  {
+    position: '9',
+    name: 'Santhanam Vidhyalaya',
+    points: '55',
+    events: [
+      { name: 'Shortfilm - III', points: '10' },
+      { name: 'Dance - III', points: '15' },
+      { name: 'Spoof - I', points: '30' },
+    ],
+  },
+  {
+    position: '10',
+    name: 'Kamakoti international School',
+    points: '50',
+    events: [
+      { name: 'Dance - I', points: '25' },
+      { name: 'Singing - II', points: '25' },
+    ],
+  },
+  {
+    position: '11',
+    name: 'Jayendra Matric School',
+    points: '45',
+    events: [
+      { name: 'Drawing - III', points: '15' },
+      { name: 'Cooking - I', points: '25' },
+      { name: 'Title event - I', points: '5' },
+    ],
+  },
+  {
+    position: '12',
+    name: 'Saint John Vestry HSS School',
+    points: '40',
+    events: [
+      { name: 'Drama - I', points: '25' },
+      { name: 'Quiz - III', points: '10' },
+      { name: 'Spoof - III', points: '5' },
+    ],
+  },
+];
+
+export default data;
