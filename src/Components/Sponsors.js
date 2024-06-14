e4e8207642dea8671e3fc77953e3eebe0104db3a
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-06-14 22:01:16 +0530

    Update Sponsors.js

--- src/Components/Sponsors.js
+++ src/Components/Sponsors.js
@@ -1,30 +1,29 @@
-function Sponsors()
+function Contact()
 {
-    return <div className='homesabout'>
-            <h2 style={{fontSize:'50px'}}>We thank our sponsors !</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ac feugiat sed lectus vestibulum mattis ullamcorper. Egestas erat imperdiet sed euismod nisi porta lorem mollis. 
-            Facilisis sed odio morbi quis commodo odio aenean sed. Tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse. 
-            Eu feugiat pretium nibh ipsum consequat. Ut consequat semper viverra nam libero justo laoreet sit. 
-            Tristique et egestas quis ipsum suspendisse.
-             Nisi lacus sed viverra tellus in hac habitasse platea dictumst. Eleifend quam adipiscing vitae proin.</p>
-             <div className="sponsorgrid">
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-                <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
-            </div>
-             </div>
+    const ContactCard = ({orgname,designation,number,image}) => {
+            return(
+            <div className="contact-card">
+            <h2>{orgname}</h2>
+            <p>{designation}</p>
+            <p>{number}</p>
+          </div>
+      );
+    }
+    return <div>
+         <h1 className="Leaderboard">Our organizers are just a call away!</h1>
+         <div className="contactgrid">
+
+        <ContactCard orgname="Organizer" designation="Overall Event Coordinator" number="9123456789"/>
+        <ContactCard orgname="Organizer" designation="Logistics Manager" number="9234567890" />
+        <ContactCard orgname = "Organizer" designation= "Overall Technical Coordinator" number = "8220532903"/>
+        <ContactCard orgname="Organizer" designation="Content Writer" number="9345678901" />
+        <ContactCard orgname="Organizer" designation="Overall Media Coordinator" number="9456789012"/>
+        <ContactCard orgname="Organizer" designation="Dance event" number="9567890123" />
+        <ContactCard orgname="Organizer" designation="singing event" number="9678901234" />
+        <ContactCard orgname="Organizer" designation="Drama event" number="9789012345" />
+        <ContactCard orgname="Organizer" designation="Spoof event" number="9890123456" />
+
+        </div>
+    </div>
 }
-export default Sponsors;
+export default Contact;
