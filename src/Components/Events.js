302de588fd4d77ebc472ea5ccac14f9162c4cab3
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-06-15 12:34:40 +0530

    Update Events.js

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,7 +1,8 @@
 function Events()
 {
     
-    return <div className="eventflex">
+    return <div>
+    <div className="eventflex">
     <h1 className="eventwelcome">Welcome to Vinterbash Events</h1>
     <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
     totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
@@ -23,9 +24,12 @@ function Events()
       <li>Event name</li>
       <li>Event name</li>
     </ol>
-    <a className="btn btn-primary" href="https://drive.google.com/file/d/14tGu8e2BPhTovnCOWR74FQbGNnueJ_WB/view?usp=drivesdk" 
+    </div>
+    <div className="eventbuttonflex">
+    <a className="btn btn-primary register" href="https://drive.google.com/file/d/14tGu8e2BPhTovnCOWR74FQbGNnueJ_WB/view?usp=drivesdk" 
     target="_blank" rel="noreferrer">Click here for Rule Book</a>
     <a className="btn btn-danger" href="registerform">Click here to Register</a>
+    </div>
     </div>
 }
 export default Events;
