cc072fafe8cc14f08dff736e5fc30e508a366998
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-06-19 10:12:12 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function Navb() {
-  const location = useLocation();
-  const showBrand = location.pathname !== '/';
   const [isToggled, setIsToggled] = useState(false);
   const handleToggle = () => {
     setIsToggled(!isToggled);
@@ -16,18 +14,19 @@ function Navb() {
     <>
       <Navbar expand="lg" className="bg-body-tertiary custom-navbar">
         <Container>
-          {showBrand && (
+         
             <Navbar.Brand href="/" className="custom-brand">
               <div>
                 <img src={require('../assets/vinterlogo.jpg')} alt='Osa logo' className="brand-logo" />
               </div>
             </Navbar.Brand>
-          )}
+          
           <Navbar.Toggle aria-controls="basic-navbar-nav" className="ml-auto custom-toggle" onClick={handleToggle}>
             <span className='toggleletters'>{isToggled ? 'X Close' : ' Menu'}</span>
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
+              <Nav.Link className="navele" id="navele1" href="/">HOME</Nav.Link>
               <Nav.Link className="navele" id="navele1" href="/events">EVENTS</Nav.Link>
               <Nav.Link className="navele" id="navele2" href="/leaderboard">LEADERBOARD</Nav.Link>
               <Nav.Link className="navele" id="navele3" href="/Sponsors">OUR SPONSORS</Nav.Link>
