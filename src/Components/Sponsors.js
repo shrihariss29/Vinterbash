aece359092b601bc48e9aaa6bd808063011e5edc
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-06-19 22:07:38 +0530

    Update Sponsors.js

--- src/Components/Sponsors.js
+++ src/Components/Sponsors.js
@@ -2,12 +2,11 @@ function Sponsors()
 {
     return <div className='homesabout'>
             <h2 style={{fontSize:'50px'}}>We thank our sponsors !</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ac feugiat sed lectus vestibulum mattis ullamcorper. Egestas erat imperdiet sed euismod nisi porta lorem mollis. 
-            Facilisis sed odio morbi quis commodo odio aenean sed. Tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse. 
-            Eu feugiat pretium nibh ipsum consequat. Ut consequat semper viverra nam libero justo laoreet sit. 
-            Tristique et egestas quis ipsum suspendisse.
-             Nisi lacus sed viverra tellus in hac habitasse platea dictumst. Eleifend quam adipiscing vitae proin.</p>
+            <p>We wholeheartedly extend our gratitude to our sponsors who have been our pillar of strength in making Vinterbash a grand
+            success. As we celebrate the achievements and growth of the students across various extracurricular and co-curricular events,
+            your unwavering support and belief in our vision has empowered us to create an unforgettable experience for students, teachers,
+            and organizers alike. Thank you once again for being an integral part of Vinterbash 2024. Your partnership continues to make a 
+            profound impact on our community and beyond.</p>
              <div className="sponsorgrid">
                 <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
                 <img src={require("../assets/Sponsor.jpg")} alt="Sponsor"/>
