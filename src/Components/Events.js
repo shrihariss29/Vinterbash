2d267de6a1f88117fd33cb2ac88b2492b193c757
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-06-29 10:59:44 +0530

    Update Events.js

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -3,32 +3,44 @@ function Events()
     
     return <div>
     <div className="eventflex">
-    <h1 className="eventwelcome">Welcome to Vinterbash Events</h1>
+    <h1 className="eventwelcome"  >Welcome to Vinterbash Events</h1>
     <p>Whether you're curious about our lineup of exciting events, eager to delve into the intricacies of the game rules, or ready to effortlessly register, we have you covered! 
     Stay updated and prepare to unleash your creativity, wits and fun side as D-day gets closer. Watch this space for more details. </p>
     <ol>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
-      <li>Event name</li>
+      <li>The Triquizzard Tournument</li>
+      <li>Itihasas Quiz</li>
+      <li>'What if?' Odyssey</li>
+      <li>Think On Your Feet</li>
+      <li>மொழியமுதம் (Mozhiyamudham)</li>
+      <li>Cartoon Verse Crossover</li>
+      <li>Multi Media Madness</li>
+      <li>Nalamdhaanaa?!</li>
+      <li>Five.Six.Seven.Eight..</li>
+      <li>Heels In The Sky</li>
+      <li>The Imitation Game - Improv</li>
+      <li>Aaathi, Idhu Adhula?! - Spoof Show</li>
+      <li>Thirai@480°</li>
+      <li>Chordially Yours</li>
+      <li>Acoustic Nirvana</li>
+      <li>Python Prowess Quest : The Logical Odyssey</li>
+      <li>Vinter Kick-Off : 5-A Side Football</li>
+      <li>Vinter Bowl-out : Turf Cricket</li>
     </ol>
     </div>
     <div className="eventbuttonflex">
-    <a className="btn btn-primary register" href="https://drive.google.com/file/d/14tGu8e2BPhTovnCOWR74FQbGNnueJ_WB/view?usp=drivesdk" 
-    target="_blank" rel="noreferrer">Click here for Rule Book</a>
-    <a className="btn btn-danger" href="registerform">Click here to Register</a>
+    <div className="rulebook">
+    <p>Click here for the Official Rule book for Vinterbash '24</p>
+    <a className="btn btn-primary register" href="https://drive.google.com/file/d/1EFlQN8PglWIVSN_fElxVsK6pqAz7EFFM/view?usp=drivesdk" 
+    target="_blank" rel="noreferrer">Rule Book</a>
+    </div>
+    <div className="rulebook">
+    <p>Click here to register online (Highly Recommended)</p>
+    <a className="btn btn-danger" href="registerform">Register</a>
+    </div>
+    <div className="rulebook">
+    <p>Click here to register in a traditional way (Alternative)</p>
+    <a className="btn btn-danger" href="https://forms.gle/oBiqhWX3nfCDRVuy6">Register</a>
+    </div>
     </div>
     </div>
 }
