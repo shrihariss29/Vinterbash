55223beddf010bdc25148e1839fa75ed2877d3fd
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-06-30 18:34:18 +0530

    Update Schedule.js

--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -25,7 +25,7 @@ function Schedule() {
         {ScheduleCard('Quiz',' Triquizzard tournament','Prelims','9:30 am to 10:30 am','To be announced','quiz.jpg')}
         {ScheduleCard('Art','Multi media madness','Single shot event','10:00 am to 01:00 pm','To be announced','art.jpg')}
         {ScheduleCard('English literature','"What if" Odyssey','Single shot event','10:00 am to 12 noon','To be announced','elits.jpg')}
-        {ScheduleCard('Tech','Python prowess quiz','Prelims','10:00 am to 11:00 am','To be announced','quiz.jpg')}
+        {ScheduleCard('Tech','Python prowess quest','Prelims','10:00 am to 11:00 am','To be announced','quiz.jpg')}
         {ScheduleCard('Dramatics','The imitation game','Prelims','11:00 am to 12 noon','To be announced','drama.jpg')}
         {ScheduleCard('தமிழ் இலக்கியம்','மொழியமுதம் ','Prelims/தகுதிச்சுற்று','11:30 am to 01:00 pm','To be announced','tamillits.jpg')}
         {ScheduleCard('Dance','Nalamdhaana','Single shot event','11:30 am to 01:30 pm','To be announced','dance.jpg')}
@@ -40,7 +40,7 @@ function Schedule() {
         {ScheduleCard('Quiz','Ithihaas Quiz','Prelims','02:00 pm to 03:00 pm','To be announced','quiz.jpg')}
         {ScheduleCard('Art','Cartoon Universe Crossover','Single shot event','02:00 pm to 05:00 pm','To be announced','art.jpg')}
         {ScheduleCard('English literature','Think on your Feet','Prelims','02:00 pm to 03:00 pm','To be announced','elits.jpg')}
-        {ScheduleCard('Tech','Python prowess quiz','Finals','02:00 pm to 04:00 pm','To be announced','quiz.jpg' )}
+        {ScheduleCard('Tech','Python prowess quest','Finals','02:00 pm to 04:00 pm','To be announced','quiz.jpg' )}
         {ScheduleCard('தமிழ் இலக்கியம்','மொழியமுதம் ','Finals/இறுதிச்சுற்று ','02:30 am to 04:30 pm','To be announced','tamillits.jpg')}
         {ScheduleCard('Quiz','Ithihaas Quiz','Finals','03:30 pm to 05:30 pm','To be announced','quiz.jpg')}
         {ScheduleCard('English literature','Think on your Feet','Prelims','03:30 pm to 05:30 pm','To be announced','elits.jpg')}
