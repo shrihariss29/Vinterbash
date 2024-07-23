3486c3221b928c40b46796729421b167a4c95492
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-07-23 16:49:29 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -33,12 +33,32 @@ function Home() {
         </p>
       </div>
       <div className='homesabout'>
-            <h2>We thank our sponsors!</h2>
+            <h2>We thank our sponsors !</h2>
             <p>We wholeheartedly extend our gratitude to our sponsors who have been our pillar of strength in making Vinterbash a grand
             success. As we celebrate the achievements and growth of the students across various extracurricular and co-curricular events,
             your unwavering support and belief in our vision has empowered us to create an unforgettable experience for students, teachers,
             and organizers alike. Thank you once again for being an integral part of Vinterbash 2024. Your partnership continues to make a 
             profound impact on our community and beyond.</p>
+             <div className="sponsorgrid">
+                <img src={require("../assets/Sponsors/pothys.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/karurvysyabank.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/Indianbank.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/madappalli.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/crysalis.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/trichybookhouse.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/scribbles.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/neverskip.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/cloudreign.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/grt.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/sribalaji.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/ultrasystems.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/bksecurity.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/feathers.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/janarthansystems.jpg")} alt="Sponsor"/>
+                <img src={require("../assets/Sponsors/srikrishnastudio.jpg")} alt="Sponsor"/>
+                
+                
+            </div>
              </div>
     </div>
   );
