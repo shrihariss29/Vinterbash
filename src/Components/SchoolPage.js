6e2958262823357e071f023f888e9c6a530b761c
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-07-25 11:22:27 +0530

    Update SchoolPage.js

--- src/Components/SchoolPage.js
+++ src/Components/SchoolPage.js
@@ -1,24 +1,68 @@
-
-
+import srivv from "../assets/pic2.jpg"
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const schoolLinks = {
-    "Sri Vageesha Vidhyashram": 'https://drive.google.com/file/d/1GnkYNO1oSirCotaWj6zPIqUOLLTDl2az/view?usp=drive_link',
-    "Sri Vignesh Vidhyalaya": 'https://example.com/vignesh',
-    "Sri Akilandeswari vidyalaya" : 'https://example.com/SAV', 
-    "RSK higher secondary school" : 'https://example.com/RSK', 
-    "Alpha Plus Matriculation Higher Secondary School" : 'https://example.com/ALpha',
-    "ST. JOAN OF ARC INTERNATIONAL SCHOOL" : 'https://example.com/STjoan',
-    "Brindavan Vidyalaya" : 'https://example.com/Brindhavan', 
-    "Chinmaya Vidhyalaya" : 'https://example.com/CHinmaya',
-    "Sri Vignesh Public School" : 'https://example.com/Vigneshpublic',
-    "Santhanam Vidyalaya" : 'https://example.com/Santhanam', 
-    "Aurobindo International School" : 'https://example.com/Aurobindo',
-    "Mahathma Gandhi Centenary Vidhyalaya" : 'https://example.com/MGCV',
-    "Nava Barath Vidhyalaya" : 'https://example.com/Navabharath',
-    "Kamala Niketan Montessari School" : 'https://example.com/knms',
-    "Alpha Cambridge International" : 'https://example.com/alpha'
+    "Sri Vageesha Vidhyashram": {
+        driveLink: 'https://drive.google.com/file/d/1GnkYNO1oSirCotaWj6zPIqUOLLTDl2az/view?usp=drive_link',
+        image: srivv
+    },
+    "Sri Vignesh Vidhyalaya": {
+        driveLink: 'https://example.com/vignesh',
+        image: 'https://via.placeholder.com/150?text=Sri+Vignesh'
+    },
+    "Sri Akilandeswari vidyalaya" : {
+        driveLink: 'https://example.com/SAV',
+        image: 'https://via.placeholder.com/150?text=Sri+Akilandeswari'
+    }, 
+    "RSK higher secondary school" : {
+        driveLink: 'https://example.com/RSK',
+        image: 'https://via.placeholder.com/150?text=RSK'
+    }, 
+    "Alpha Plus Matriculation Higher Secondary School" : {
+        driveLink: 'https://example.com/ALpha',
+        image: 'https://via.placeholder.com/150?text=Alpha'
+    },
+    "ST. JOAN OF ARC INTERNATIONAL SCHOOL" : {
+        driveLink: 'https://example.com/STjoan',
+        image: 'https://via.placeholder.com/150?text=ST+Joan'
+    },
+    "Brindavan Vidyalaya" : {
+        driveLink: 'https://example.com/Brindhavan',
+        image: 'https://via.placeholder.com/150?text=Brindavan'
+    }, 
+    "Chinmaya Vidhyalaya" : {
+        driveLink: 'https://example.com/CHinmaya',
+        image: 'https://via.placeholder.com/150?text=Chinmaya'
+    },
+    "Sri Vignesh Public School" : {
+        driveLink: 'https://example.com/Vigneshpublic',
+        image: 'https://via.placeholder.com/150?text=Sri+Vignesh+Public'
+    },
+    "Santhanam Vidyalaya" : {
+        driveLink: 'https://example.com/Santhanam',
+        image: 'https://via.placeholder.com/150?text=Santhanam'
+    }, 
+    "Aurobindo International School" : {
+        driveLink: 'https://example.com/Aurobindo',
+        image: 'https://via.placeholder.com/150?text=Aurobindo'
+    },
+    "Mahathma Gandhi Centenary Vidhyalaya" : {
+        driveLink: 'https://example.com/MGCV',
+        image: 'https://via.placeholder.com/150?text=MGCV'
+    },
+    "Nava Barath Vidhyalaya" : {
+        driveLink: 'https://example.com/Navabharath',
+        image: 'https://via.placeholder.com/150?text=Navabharath'
+    },
+    "Kamala Niketan Montessari School" : {
+        driveLink: 'https://example.com/knms',
+        image: 'https://via.placeholder.com/150?text=KNMS'
+    },
+    "Alpha Cambridge International" : {
+        driveLink: 'https://example.com/alpha',
+        image: 'https://via.placeholder.com/150?text=Alpha+Cambridge'
+    }
 };
 
 const SchoolPage = () => {
@@ -31,14 +75,17 @@ const SchoolPage = () => {
         return null;
     }
 
-    const driveLink = schoolLinks[school];
+    const { driveLink, image } = schoolLinks[school] || {};
 
     return (
         <div className='schoolimage'>
-            <h1 className='Leaderboard'>{school}</h1>
-            <p>We have given the download link below. As the pictures are of highest quality, the file size will be large</p>
+            {image && <img src={image} alt={school} />}
+            <h1 className='Leaderboard'>Thank you, {school}</h1>
+            <p>Your support and cooperation has been immense for this edition of vinterbash. We were much elated with your presence.
+            So we have tried our best to give you the moments you spent with us. This is a new effort from us, doing it for first time in Trichy.
+            We have given the download link below. As the pictures are of highest quality, the file size will be large</p>
             {driveLink ? (
-                <a href={driveLink} className="btn btn-primary" target='blank'>Download</a>
+                <a href={driveLink} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Download</a>
             ) : (
                 <p>Photos will be coming soon</p>
             )}
