b5a1caf99a8f9aa379eb18a2a792629ffff7ae40
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-07-26 13:51:56 +0530

    Update data.js

--- src/Components/data.js
+++ src/Components/data.js
@@ -1,7 +1,142 @@
 const data = [
   {
-    position: 'x',
-    name: 'School name',
+    position: '0',
+    name: 'Sri Vignesh Vidhyalaya',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'Sri Akilandeswari Vidhyalaya',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'RSK Higher Secondary School',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'Alpha Plus Matriculation Higher Secondary School',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'ST. JOAN OF ARC INTERNATIONAL SCHOOL',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'Brindavan Vidyalaya',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'Chinmaya Vidhyalaya',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'Sri Vignesh Public School',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'Santhanam Vidyalaya',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'Aurobindo International School',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'Mahathma Gandhi Centenary Vidhyalaya',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'Nava Bharath Vidhyalaya',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'Kamala Niketan Montessari School',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'Alpha Cambridge International',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'Sri Vageesha Vidhyashram',
+    points: '000',
+    events: [
+      { name: 'Event - I', points: '00' },
+    ],
+  },
+
+  {
+    position: '0',
+    name: 'Sri Vignesh Vidhyalaya',
     points: '000',
     events: [
       { name: 'Event - I', points: '00' },
