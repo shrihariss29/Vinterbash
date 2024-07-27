68590ebb7d6a475436ed76ba140a076a35a231f5
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-07-27 11:43:13 +0530

    Update scoredata.js

--- src/Components/scoredata.js
+++ src/Components/scoredata.js
@@ -2,23 +2,31 @@
 
 const cricketMatches = [
   {
-    status: "Group stage 6 over match (Live)",
+    status: "Group stage 6 over match (Completed)",
     color: "#E76F51",
     school1Name: "Brindhavan Vidhyalaya",
     school1Score: "47/1 (6)",
     school2Name: "St.Joan of Arc International School (Toss)",
     school2Score: "48/2(4.5)",
-    manOfTheMatch: "melfi joshua"
+    manOfTheMatch: ""
   }
 ];
 
 const footballMatches = [
   {
-    status: "Group stage game (Live)",
+    status: "Group stage game (Completed)",
     color: "#E76F51",
     school1Name: "Vageesha Vidhyashram",
     schoolsScore: "5-0 (FT)",
     school2Name: "Sri Akilandeshwari Vidhyalaya",
+    manOfTheMatch: ""
+  },
+  {
+    status: "Group stage game (Live)",
+    color: "#E76F51",
+    school1Name: "RSK Higher secondary school",
+    schoolsScore: "2-0 (HT)",
+    school2Name: "Boys Higher Secondary School",
     manOfTheMatch: ""
   },
   
