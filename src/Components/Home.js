58d115c9da90a42eafc309d1f3812541e93cbb0c
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-07-27 16:39:19 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,25 +5,7 @@
         <div id="homepage">
           <h1 id="Vinter">V I N T E R B A S H</h1>
           <h3 id="theme">WHERE CHAMPIONS RISE!</h3>
-        </div>
-        <h2 className="hometitle">Our OSA Thanks you!</h2>
-          <p className="homeabout">
-          We extend our heartfelt gratitude to all stakeholders who contributed to the resounding success of Vinterbash 2024. We appreciate the enthusiastic participation of school management, representative teachers, and students, who all responded with remarkable zeal. Our sincere thanks also go to our esteemed alumni, who organized the event with seamless efficiency. Lastly, we are indebted to the management of Sri Vageesha Vidhyashram for their unwavering support and encouragement throughout. We look forward to nurturing and strengthening these bonds in future editions of Vinterbash.
-          </p>
-        <div class='Thanks'>
-        <div className="websitepeople">
-        <h1 class='team'>Team behind this website : </h1>
-        <a className="btn btn-primary" href="https://www.instagram.com/shrihari.29/" target="_blank">Shrihari.S - Web Developer</a>
-        <a className="btn btn-primary">Kaavia.N - COontent writer</a>
-        <a className="btn btn-primary">Sabarigirsan - Events logo</a>
-        </div>
-        <div className="websitepeople">
-        <h1 class='team'>Special thanks to :</h1>
-        <a className="btn btn-primary">Srinivasa Raghavan.P</a>
-        <a className="btn btn-primary">Srivasthan</a>
-        <a className="btn btn-primary">Amrutha Varshan</a>
-        </div>
-        </div>
+        
         <Carousel className="carousel" interval={4000} wrap={true}>
           {[1, 2, 3, 4, 5, 6, 7].map((index) => (
             <Carousel.Item key={index}>
