42be39ce7870745ab1fc437aebc837124f3b5e05
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-07-27 12:21:12 +0530

    Update scoredata.js

--- src/Components/scoredata.js
+++ src/Components/scoredata.js
@@ -2,7 +2,7 @@
 
 const cricketMatches = [
   {
-    status: "Group stage 6 over match (Completed)",
+    status: "Group stage 6 over match - 1 (Completed)",
     color: "#E76F51",
     school1Name: "Brindhavan",
     school1Score: "47/1 (6)",
@@ -10,7 +10,7 @@ const cricketMatches = [
     school2Score: "48/2(4.5)",
   },
   {
-    status: "Group stage 6 over match (Completed)",
+    status: "Group stage 6 over match - 2 (Completed)",
     color: "#E76F51",
     school1Name: "MGCV (Won)",
     school1Score: "45-2(6)",
@@ -21,24 +21,24 @@ const cricketMatches = [
 
 const footballMatches = [
   {
-    status: "Group stage game (Completed)",
+    status: "Group stage game - 1 (Completed)",
     color: "#E76F51",
     school1Name: "Vageesha (Won)",
     schoolsScore: "5-0 (FT)",
     school2Name: "SAV",
   },
   {
-    status: "Group stage game (Completed)",
+    status: "Group stage game - 2 (Completed)",
     color: "#E76F51",
     school1Name: "RSK (Won)",
     schoolsScore: "2-0 (FT)",
     school2Name: "Srirangam boys",
   },
   {
-    status: "Group stage game (Completed)",
+    status: "Group stage game - 3 (Completed)",
     color: "#E76F51",
     school1Name: "Santhanam (Won)",
-    schoolsScore: "2-0 (HT)",
+    schoolsScore: "3-0 (FT)",
     school2Name: "Chinmaya",
   },
   
