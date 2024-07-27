0ac310b192f4bf0fda4435d914b3ce3157e5e127
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-07-27 11:15:30 +0530

    Update scoredata.js

--- src/Components/scoredata.js
+++ src/Components/scoredata.js
@@ -2,41 +2,24 @@
 
 const cricketMatches = [
     {
-      status: "Group stage 6 over match (Completed)",
-      color: "#9BEC00",
-      school1Name: "School (Toss)",
-      school1Score: "xx/x(x)",
-      school2Name: "School",
-      school2Score: "xx/x(x)",
-      manOfTheMatch: "Student()"
-    },
-    {
       status: "Group stage 6 over match (Live)",
       color: "#E76F51",
-      school1Name: "School",
-      school1Score: "xx/x(x)",
-      school2Name: "School(Toss)",
+      school1Name: "Brindhavan Vidhyalaya",
+      school1Score: "47/1 (6)",
+      school2Name: "St.Joan of Arc International School (Toss)",
       school2Score: "Yet to bat",
-      manOfTheMatch: "Student()"
+      manOfTheMatch: ""
     }
   ];
   
   const footballMatches = [
     {
-      status: "Group stage game (Completed)",
-      color: "#9BEC00",
-      school1Name: "School",
-      schoolsScore: "x-x (FT)",
-      school2Name: "School",
-      manOfTheMatch: "Student()"
-    },
-    {
       status: "Group stage game (Live)",
       color: "#E76F51",
-      school1Name: "School",
-      schoolsScore: "x-x (HT)",
-      school2Name: "School",
-      manOfTheMatch: "Student()"
+      school1Name: "Vageesha Vidhyashram",
+      schoolsScore: "3-0 (HT)",
+      school2Name: "Sri Akilandeshwari Vidhyalaya",
+      manOfTheMatch: ""
     },
     
   ];
